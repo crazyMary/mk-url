4959f21d944468665e9615cb9caac0bfd0f3f62e
liuxiang <350646436@qq.com>
2020-03-02 15:28:43 +0800

    export hash

--- es/index.js
+++ es/index.js
@@ -2,3 +2,5 @@ import _parse from './parse';
 export { _parse as parse };
 import _search from './search';
 export { _search as search };
+import _hash from './hash';
+export { _hash as hash };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export parse from './parse'
 export search from './search'
+export hash from './hash'
